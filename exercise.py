6e5f3533bf8542a8d35d9bb863e4ad14b27eddc7
Dashazem <dashazemska@gmail.com>
2024-04-03 18:14:45 +0200

    first commit

---
+++ exercise.py
@@ -0,0 +1,10 @@
+class Usuario:
+
+    def __init__(self, nombre, contraseña):
+        self.nombre = nombre
+        self.contraseña = contraseña
+
+    
+usr = Usuario('Dasha', 'krylchuk123')
+print(usr)
+
